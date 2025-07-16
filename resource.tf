ed1bea277e66ebebe1c1d8dd595715af7e9024fb
krupasandhya <nandivadasanju2@gmail.com>
2025-07-16 19:03:27 +0000

    new

--- resource.tf
+++ resource.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "terra" {
   name     = "terra"
-  location = "East US"
+  location = "Central US"
 }
 resource "azurerm_virtual_network" "terra_vnet" {
     name                = "terra-vnet"
